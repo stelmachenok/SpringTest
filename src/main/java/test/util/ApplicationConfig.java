b586d1bca7a93c2a01c150b47a0f3d311e1248f8
Max <stelmachenokm@gmail.com>
2017-07-13 21:52:05 +0300

    Add test.jsp, configs and controller

---
+++ src/main/java/test/util/ApplicationConfig.java
@@ -0,0 +1,29 @@
+package test.util;
+
+/**
+ * Created by y50-70 on 12.07.2017.
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class ApplicationConfig extends WebMvcConfigurerAdapter{
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
